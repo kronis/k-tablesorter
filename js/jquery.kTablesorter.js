289f29ef85a46c7de542409625725f854b1d8ddc
Markus Loupeen <mal@capraconsulting.no>
2012-09-02 22:17:44 +0200

    Sorting working with diffrent parsers

--- js/jquery.kTablesorter.js
+++ js/jquery.kTablesorter.js
@@ -3,25 +3,34 @@
 	var dataName = "kSorter";
 	var columnIndexName = "kSorterColumnIndex";
 
+	var sorters = [];
+
 	// Default settings for kTablesorter
 	var defaults = {
-		debug: true
+		debug: false,
+		rowsToDetermineSorter: 1
 	};
 
-	function mySorter(column, order) {
+	function addSorter(sorter) {
+		sorters.push(sorter);
+	}
+
+	function mySorter(column, order, sorter) {
 		return function (a, b) {
+			var result = sorter(a[column], b[column]);
+
 			if (order === "asc") {
-				return a[column] - b[column];
+				return sorter(a[column], b[column]);
 			}
-			return a[column] + b[column];
+			return (-sorter(a[column], b[column]));
 		};
 	}
 
 	function bindHeaders(headers) {
 		for (var i = 0; i < headers.length; i++) {
 			var header = headers[i];
-			$(header).unbind('click').click(function() {
-				
+			$(header).unbind('click').click(function () {
+
 				// Get table (should be optimized)
 				var table = $(this).parent().parent().parent();
 
@@ -29,16 +38,16 @@
 				var data = $(table).data(dataName);
 
 				// Get sorting
-				var sorting = data.sorting;
+				var sortingSettings = data.sortingSettings;
 
 				// Get header index
 				var headerIndex = $(this).data(columnIndexName);
 
 				// Update sorting and save it
-				if (sorting.column === headerIndex) {
-					sorting.order = (sorting.order === "asc" ? "desc" : "asc");
+				if (sortingSettings.column === headerIndex) {
+					sortingSettings.order = (sortingSettings.order === "asc" ? "desc" : "asc");
 				} else {
-					sorting.column = headerIndex;
+					sortingSettings.column = headerIndex;
 				}
 				$(table).data(dataName, data);
 
@@ -59,6 +68,89 @@
 		}
 	}
 
+	function initStandardSorters() {
+		var sorterText = {
+			name: "text",
+			format: function () {},
+			test: function (string) {
+				return true;
+			},
+			sorter: function (a, b) {
+				return a.toLowerCase() > b.toLowerCase() ? 1 : -1;
+			}
+		};
+		var sorterInt = {
+			name: "int",
+			format: function () {},
+			test: function (string) {
+				return (/^\d*$/g).test(string);
+			},
+			sorter: function (a, b) {
+				return a - b;
+			}
+		};
+		addSorter(sorterText);
+		addSorter(sorterInt);
+	}
+
+	function evaluateColumnType(types) {
+		// If no types in array, then treat is as 'text'
+		if (types.length === 0) {
+			return "text";
+		}
+
+		// Create a temporary array that will help us (should only contains one value)
+		var uniqueValues = [];
+
+		// Add first type to teporary array
+		uniqueValues.push(types[0]);
+
+		for (var i = 1; i < types.length; i++) {
+			var type = types[i];
+
+			// If type exists in temporary array, continue
+			if (uniqueValues.indexOf(type) === 0) {
+				continue;
+			}
+
+			// If more then one type, treat is as 'text'
+			return "text";
+		}
+		return uniqueValues[0];
+	}
+
+	function getSorterForData(columnData) {
+		var results = [];
+
+		for (var i = 0; i < columnData.length; i++) {
+			var data = columnData[i];
+
+			for (var j = 1; j < sorters.length; j++) {
+				var sorter = sorters[j];
+				if (sorter.test(data)) {
+					results.push(sorter.name);
+					break;
+				}
+			}
+		}
+
+		return evaluateColumnType(results);
+	}
+
+	function getDataForColumn(table, headerIndex) {
+		var data = $(table).data(dataName);
+		var dataCache = data.dataCache;
+		var returnData = [];
+
+		for (var i = 0; i < dataCache.length; i++) {
+			var row = dataCache[i];
+			returnData.push(row[headerIndex]);
+
+			// TODO : implement a breake for for loop, use settings.rowsToDetermineSorter
+		}
+		return returnData;
+	}
+
 	var methods = {
 		init: function (options) {
 			return this.each(function (options) {
@@ -67,7 +159,7 @@
 					'options': '',
 					'headerCache': '',
 					'dataCache': '',
-					'sorting': ''
+					'sortingSettings': ''
 				};
 
 				// Options for table
@@ -80,26 +172,26 @@
 				data.dataCache = [[]];
 
 				// Sorting settings for table
-				data.sorting = {
+				data.sortingSettings = {
 					'column': 0,
-					'order': 'asc'
+					'order': 'asc',
 				};
 
 				// Save everything on element
 				$(this).data(dataName, data);
-
-				// Update headers
-				methods.updateHeaders.apply(this);
-				if (data.options.debug) {
-					console.log('headerCache:');
-					console.log(data.headerCache);
-				}
 
 				// Update data
 				methods.updateData.apply(this);
 				if (data.options.debug) {
 					console.log('dataCache:');
 					console.log(data.dataCache);
+				}
+
+				// Update headers
+				methods.updateHeaders.apply(this);
+				if (data.options.debug) {
+					console.log('headerCache:');
+					console.log(data.headerCache);
 				}
 
 				// Sort everyting 
@@ -128,24 +220,26 @@
 				var i;
 
 				// TODO : Unbind everything on headers (if some is removed)
+				var a = "TODO";
 
 				// Save headers in seperate cache
 				for (i = 0; i < headersLength; i++) {
 					var header = headers[i];
 
-					// Determine column type 
-					var type = 'text';
+					// Get data for a column
+					var columnData = getDataForColumn($(this), i);
+
+					// Determine column type and selecting sorter
+					var sorter = getSorterForData(columnData);
 
 					// Save header
 					data.headerCache[i] = {
 						'header': $(header).text(),
-						'type': type
+						'sorter': sorter
 					};
 
 					// Save column index to data on column
 					$(header).data(columnIndexName, i);
-
-
 				}
 				$(this).data(dataName, data);
 			});
@@ -182,7 +276,18 @@
 		sort: function () {
 			return $(this).each(function () {
 				var data = $(this).data(dataName);
-				data.dataCache.sort(mySorter(data.sorting.column, data.sorting.order));
+				var sortingSettings = data.sortingSettings;
+				var headerCache = data.headerCache;
+				var sorterName = headerCache[sortingSettings.column].sorter;
+				var sorter;
+				for (var i = 0; i < sorters.length; i++) {
+					sorter = sorters[i];
+					if (sorter.name === sorterName) {
+						break;
+					}
+				}
+
+				data.dataCache.sort(mySorter(sortingSettings.column, sortingSettings.order, sorter.sorter));
 				$(this).data(dataName, data);
 			});
 		},
@@ -194,6 +299,9 @@
 				var data = $(this).data(dataName);
 				rewriteTable($(this), data.dataCache);
 			});
+		},
+		addSorter: function (sorter) {
+			addSorter(sorter);
 		}
 	};
 
@@ -203,6 +311,7 @@
 		if (methods[method]) {
 			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
 		} else if (typeof method === 'object' || !method) {
+			initStandardSorters();
 			return methods.init.apply(this, arguments);
 		} else {
 			$.error('Method ' + method + ' does not exist on jQuery.kTablesorter');
